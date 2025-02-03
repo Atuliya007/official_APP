f67d53862cd6637d003ffd32c1a9d275db1d9101
Atuliya007 <atuliyabansal07@gmail.com>
2025-02-03 23:06:44 +0530

    temporary session added

--- APP.py
+++ APP.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 
 if __name__ == "__main__":
